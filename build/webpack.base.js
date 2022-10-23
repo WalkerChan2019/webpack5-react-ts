2f0a711816859248e1904b83f9f88579204aa534
walker <walkerchan2019@163.com>
2022-10-24 04:18:24 +0800

    wk-init

---
+++ build/webpack.base.js
@@ -0,0 +1,15 @@
+//  公共配置
+
+// webpack.base.js
+const path = require("path");
+
+module.exports = {
+  entry: path.join(__dirname, "../src/index.tsx"), // 入口文件
+  // 打包文件出口
+  output: {
+    filename: "static/js/[name].js", // 每个输出js的名称
+    path: path.join(__dirname, "../dist"), // 打包结果输出路径
+    clean: true, // webpack4需要配置clean-webpack-plugin来删除dist文件,webpack5内置了
+    publicPath: "/", // 打包后文件的公共前缀路径
+  },
+};
